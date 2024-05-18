6a91f11b4a16125996cb74b498810eb2db853579
rioxiu <richardosinulingga@outlook.jp>
2024-05-18 20:21:22 +0700

    feat: add html

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 import { Elysia } from "elysia";
 import { LinkTabel, configDB, pool } from "./config/db";
 import { createShortLinks, redirectLinks } from "./handlers/link";
+import { html } from "@elysiajs/html";
+import { staticPlugin } from '@elysiajs/static'
+import { HTMLPages } from "./html";
+
+
+const app = new Elysia()
+  .use(html())
+  .use(staticPlugin())
+
+  .get('/:shortlink', ({ params: { shortlink } }) => redirectLinks(shortlink))
+  .group('/api', api => {
+    return api
+      .post('/create-link', ({ body, set }) => createShortLinks(body as { url: string, shortlink: string }))
+  })
+
+app.get("/", () => HTMLPages())
 
 const startServer = async () => {
   try {
     await configDB()
-    const app = new Elysia()
-    app.get('/:shortlink', ({ params: { shortlink } }) => redirectLinks(shortlink))
-    app.group('/api', api => {
-      return api
-        .post('/create-link', ({ body, set }) => createShortLinks(body as { url: string, shortlink: string }))
-
-        .get('/', () => {
-          Response.json({
-            message: 'Welcome to the Link Shortener API'
-          })
-        })
-    })
     app.listen(8888, () => { console.log('Server is running on port 8888') })
   } catch (error) {
     console.log(error)
   }
 }
+
+
 await startServer()
